44d80ecdd7dcaae527935b36de40ccc2019627ac
123things <fanruili2017@163.com>
2020-08-06 12:33:09 +0800

    vue.config.js

---
+++ vue.config.js
@@ -0,0 +1,21 @@
+const path = require('path')
+function resolve(dir){
+    return path.join(__dirname, dir);
+}
+module.exports={
+    publicPath: process.env.NODE_ENV === 'production' ? './' : '/', // 根域上下文目录
+    outputDir: 'dist', // 构建输出目录
+    assetsDir: 'assets', // 静态资源目录 (js, css, img, fonts)
+    lintOnSave: false, // 是否开启eslint保存检测，有效值：ture | false | error
+    runtimeCompiler: true, // 运行时版本是否需要编译
+    transpileDependencies: [], // 默认babel-loader忽略mode_modules，这里可增加例外的依赖包名
+    productionSourceMap: false, // 是否在构建生产包时生成 sourceMap 文件，false将提高构建速度
+    chainWebpack:(config)=>{
+        config.resolve.alias
+            .set('@', resolve('src'))
+            .set('@assets', resolve('src/assets'))
+            .set('@components', resolve('src/components'))
+            .set('@views', resolve('src/views'))
+            .set('@store', resolve('src/store'));
+    }
+}
